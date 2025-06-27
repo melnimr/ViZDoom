20ead6f8841a438681d2c9f304dbb1a0339efbc6
M. Elnimr <elnimr.m@gmail.com>
2025-06-27 08:44:54 -0700

    Adding scripts to benchmark vizdoom in vectorized env.

---
+++ examples/python/gymnasium_vect_bench.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+#####################################################################
+# Test and benchmark the vectorization of VizDoom in gymnasium
+#####################################################################
+
+import time
+import argparse
+import warnings
+
+from gymnasium.vector import AsyncVectorEnv
+import gymnasium
+from vizdoom import gymnasium_wrapper
+
+warnings.filterwarnings("ignore")
+parser = argparse.ArgumentParser()
+parser.add_argument("--n_envs", type =int, default = 1, help ="Number of envs")
+args =parser.parse_args()
+seed = 42
+n_episodes = 1000
+
+# Pick an environment VizdoomCorridor-v0
+envs = gymnasium.make_vec("VizdoomCorridor-v0", num_envs =args.n_envs, vectorization_mode="async")
+
+# Time it
+start = time.time()
+
+observation, info = envs.reset()
+for _ in range(n_episodes):
+    # No learning here, for purposes of benchmarks
+    actions = envs.action_space.sample()
+    observations, rewards, terminations, truncations, infos = envs.step(actions)
+    # env reset here slows it down 
+    # if terminated or truncated:
+    #    observation, info = env.reset()
+print(f"{args.n_envs}  {n_episodes *args.n_envs /round(time.time() - start,1)}")
+envs.close()
