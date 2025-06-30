8812d43635e05c640d147229901e05d67c27f6be
M. Elnimr <elnimr.m@gmail.com>
2025-06-30 03:04:38 -0700

    Fixing pre-commit changes

--- examples/python/gymnasium_vect_bench.py
+++ examples/python/gymnasium_vect_bench.py
@@ -1,26 +1,25 @@
-#!/usr/bin/env python3
-
 #####################################################################
 # Test and benchmark the vectorization of VizDoom in gymnasium
 #####################################################################
 
-import time
 import argparse
+import time
 import warnings
 
-from gymnasium.vector import AsyncVectorEnv
 import gymnasium
-from vizdoom import gymnasium_wrapper
+
 
 warnings.filterwarnings("ignore")
 parser = argparse.ArgumentParser()
-parser.add_argument("--n_envs", type =int, default = 1, help ="Number of envs")
-args =parser.parse_args()
+parser.add_argument("--n_envs", type=int, default=1, help="Number of envs")
+args = parser.parse_args()
 seed = 42
 n_episodes = 1000
 
 # Pick an environment VizdoomCorridor-v0
-envs = gymnasium.make_vec("VizdoomCorridor-v0", num_envs =args.n_envs, vectorization_mode="async")
+envs = gymnasium.make_vec(
+    "VizdoomCorridor-v0", num_envs=args.n_envs, vectorization_mode="async"
+)
 
 # Time it
 start = time.time()
@@ -30,7 +29,7 @@ for _ in range(n_episodes):
     # No learning here, for purposes of benchmarks
     actions = envs.action_space.sample()
     observations, rewards, terminations, truncations, infos = envs.step(actions)
-    # env reset here slows it down 
+    # env reset here slows it down
     # if terminated or truncated:
     #    observation, info = env.reset()
 print(f"{args.n_envs}  {n_episodes *args.n_envs /round(time.time() - start,1)}")
