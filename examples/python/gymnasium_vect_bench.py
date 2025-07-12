a684e15c520da11cd7501a25f637d5d942aafb8b
M. Elnimr <elnimr.m@gmail.com>
2025-07-11 17:07:33 -0700

    Adding some comments and the main() function.

--- examples/python/gymnasium_vect_bench.py
+++ examples/python/gymnasium_vect_bench.py
@@ -20,21 +20,24 @@ args = parser.parse_args()
 seed = 42
 n_steps = 1000
 
-# Pick an environment VizdoomCorridor-v0
-envs = gymnasium.make_vec(
-    "VizdoomCorridor-v0", num_envs=args.n_envs, vectorization_mode=args.mode
-)
 
-# Time it
-start = time.time()
+if __name__ == "__main__":
 
-observation, info = envs.reset()
-for _ in range(n_steps):
-    # No learning here, for purposes of benchmarks
-    actions = envs.action_space.sample()
-    observations, rewards, terminations, truncations, infos = envs.step(actions)
-    # env reset here slows it down
-    # if terminated or truncated:
-    #    observation, info = env.reset()
-print(f"{args.n_envs}  {n_steps *args.n_envs /round(time.time() - start,1)}")
-envs.close()
+    # Pick an environment VizdoomCorridor-v0
+    envs = gymnasium.make_vec(
+        "VizdoomCorridor-v0", num_envs=args.n_envs, vectorization_mode=args.mode
+    )
+
+    # Time it
+    start = time.time()
+
+    observation, info = envs.reset()
+    for _ in range(n_steps):
+        # No learning here, for purposes of benchmarks
+        actions = envs.action_space.sample()
+        observations, rewards, terminations, truncations, infos = envs.step(actions)
+        # no need for env.reset() here since the default is AutoReset(https://farama.org/Vector-Autoreset-Mode)
+        # if terminated or truncated:
+        #    observation, info = env.reset()
+    print(f"{args.n_envs}  {n_steps *args.n_envs /round(time.time() - start,1)}")
+    envs.close()
